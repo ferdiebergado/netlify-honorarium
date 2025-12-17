2b4b5ff3d5030def4da9976526def3ae9f7605f5
ferdie bergado <ferdiebergado@gmail.com>
2025-12-17 11:20:22 +0800

    chore: Move netlify at the end of plugin array

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), netlify(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
