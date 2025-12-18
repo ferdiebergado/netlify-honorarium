a5d0bcc0ef3eb251a0f359ba6db4ef0ed42b61ed
ferdie bergado <ferdiebergado@gmail.com>
2025-12-18 15:02:09 +0800

    style: Align spinner and label

--- src/features/activities/ActivitiesPage.tsx
+++ src/features/activities/ActivitiesPage.tsx
@@ -1,16 +1,19 @@
-import { Spinner } from "@/components/ui/spinner";
-import type { ColumnDef } from "@tanstack/react-table";
-import { useActivityList, type Activity } from "./activity";
-import ActivityList from "./ActivityList";
-import CreateActivityForm from "./CreateActivityForm";
+import { Spinner } from '@/components/ui/spinner';
+import type { ColumnDef } from '@tanstack/react-table';
+import { useActivityList, type Activity } from './activity';
+import ActivityList from './ActivityList';
+import CreateActivityForm from './CreateActivityForm';
+import ViewActivityDialog from './ViewActivityDialog';
 
 const columns: ColumnDef<Activity>[] = [
-  { accessorKey: "title", header: "Title" },
-  { accessorKey: "venue", header: "Venue" },
-  { accessorKey: "startDate", header: "Start Date" },
-  { accessorKey: "endDate", header: "End Date" },
-  { accessorKey: "code", header: "Activity Code" },
-  { accessorKey: "fund", header: "Fund Source" },
+  { accessorKey: 'id', header: 'ID' },
+  { accessorKey: 'title', header: 'Title' },
+  { accessorKey: 'venue', header: 'Venue' },
+  { accessorKey: 'startDate', header: 'Start Date' },
+  { accessorKey: 'endDate', header: 'End Date' },
+  { accessorKey: 'code', header: 'Activity Code' },
+  { accessorKey: 'fund', header: 'Fund Source' },
+  { id: 'actions', cell: ({ row }) => <ViewActivityDialog activity={row.original} /> },
 ];
 
 export default function ActivitiesPage() {
@@ -20,13 +23,10 @@ export default function ActivitiesPage() {
     <>
       <h1 className="text-3xl font-bold">Activities</h1>
       <h2 className="text-muted-foreground">List of Activities</h2>
-
       <CreateActivityForm />
-      {isError && (
-        <p className="m-3 text-destructive">Error: {error.message}</p>
-      )}
+      {isError && <p className="m-3 text-destructive">Error: {error.message}</p>}
       {isPending ? (
-        <div className="flex m-3 gap-3">
+        <div className="flex m-3 gap-3 items-center">
           <Spinner /> Fetching activities...
         </div>
       ) : (
