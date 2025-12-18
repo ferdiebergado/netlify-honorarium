40432ffa8e5a68e934c98c97222942e44df9c771
ferdie bergado <ferdiebergado@gmail.com>
2025-12-18 22:01:55 +0800

    chore: Install react-x and react-dom eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import reactDom from 'eslint-plugin-react-dom';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from 'eslint-plugin-react-x';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
-import prettierTailwind from 'prettier-plugin-tailwindcss';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
@@ -13,15 +14,20 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       eslintConfigPrettier,
-      prettierTailwind,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ]);
