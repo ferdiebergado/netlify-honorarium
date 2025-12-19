84b14e37a4005bcb7bbc4817f8bd1216276211dc
ferdie bergado <ferdiebergado@gmail.com>
2025-12-19 10:55:34 +0800

    refactor: Manually toggle the dialog

--- src/features/activities/CreateActivityForm.tsx
+++ src/features/activities/CreateActivityForm.tsx
@@ -5,31 +5,39 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
 import { CirclePlus } from 'lucide-react';
-import { useActivityForm, useCreateActivity, type ActivityFormdata } from './activity';
+import { useState } from 'react';
+import { type ActivityFormdata } from './activity';
 import ActivityForm from './ActivityForm';
 
 export default function CreateActivityForm() {
-  const form = useActivityForm();
-  const { isPending, mutate } = useCreateActivity(form);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const formData: ActivityFormdata = {
+    title: '',
+    venueId: 0,
+    startDate: '',
+    endDate: '',
+    code: '',
+    fund: '',
+    focalId: 0,
+  };
 
-  const onSubmit = (data: ActivityFormdata) => mutate(data);
+  const handleClick = () => {
+    setIsDialogOpen(true);
+  };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button size="lg" className="bg-cyan-500">
-          <CirclePlus /> New Activity
-        </Button>
-      </DialogTrigger>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <Button size="lg" className="bg-cyan-500" onClick={handleClick}>
+        <CirclePlus /> New Activity
+      </Button>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>New Activity</DialogTitle>
           <DialogDescription>Create a new activity.</DialogDescription>
         </DialogHeader>
-        <ActivityForm form={form} isLoading={isPending} onSubmit={onSubmit} />
+        <ActivityForm data={formData} setIsDialogOpen={setIsDialogOpen} />
       </DialogContent>
     </Dialog>
   );
