2217e474f6d2df5675f9693ddadc35c18462e22c
ferdie bergado <ferdiebergado@gmail.com>
2025-12-19 16:55:40 +0800

    feat: Add sidebar

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,60 @@
+import { Calendar1, Home, Settings } from 'lucide-react';
+import type { FC } from 'react';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from './ui/sidebar';
+
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: Home,
+  },
+  {
+    title: 'Activities',
+    url: '/activities',
+    icon: Calendar1,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+];
+
+const AppSidebar: FC = () => {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map(item => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router';
-import Navbar from './Navbar';
+import AppSidebar from './AppSidebar';
+import { SidebarProvider, SidebarTrigger } from './ui/sidebar';
 import { Toaster } from './ui/sonner';
 
 export default function Layout() {
   return (
-    <>
-      <Navbar />
-      <main className="m-12">
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="mx-6">
+        <SidebarTrigger className="my-3" />
         <Outlet />
         <Toaster position="top-right" expand richColors />
       </main>
-    </>
+    </SidebarProvider>
   );
 }
