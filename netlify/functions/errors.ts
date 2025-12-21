5e9f86dec53c8e6fdf259ca49c959b82d9b54088
ferdie bergado <ferdiebergado@gmail.com>
2025-12-21 21:24:48 +0800

    refactor: Create custom http error

--- netlify/functions/errors.ts
+++ netlify/functions/errors.ts
@@ -1,18 +1,38 @@
-export class ValidationError extends Error {
+class HTTPError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, msg: string) {
+    super(msg);
+    this.statusCode = statusCode;
+
+    Object.setPrototypeOf(this, HTTPError.prototype);
+  }
+}
+
+export class ValidationError extends HTTPError {
   readonly name = 'ValidationError';
-  statusCode = 400;
 
   constructor() {
-    super('Invalid input');
+    super(400, 'Invalid input');
 
     Object.setPrototypeOf(this, ValidationError.prototype);
   }
 }
 
-export function handleError(error: unknown) {
+export class NotFoundError extends HTTPError {
+  readonly name = 'NotFoundError';
+
+  constructor() {
+    super(404, 'Resource not found.');
+
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+}
+
+export function errorResponse(error: unknown) {
   console.error(error);
 
-  if (error instanceof ValidationError)
+  if (error instanceof HTTPError)
     return Response.json({ message: error.message }, { status: error.statusCode });
 
   return Response.json({ message: 'An unknown error occurred.' }, { status: 500 });
