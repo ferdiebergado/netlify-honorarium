b7a4a9e6702158c5d757534f443a1b899732cf68
ferdie bergado <ferdiebergado@gmail.com>
2025-12-24 07:37:23 +0800

    refactor: Create parseId helper

--- netlify/functions/db.ts
+++ netlify/functions/db.ts
@@ -1,6 +1,14 @@
-import { createClient } from "@libsql/client";
+import { createClient } from '@libsql/client';
 
 export const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL ?? "file:local.db",
+  url: process.env.TURSO_DATABASE_URL ?? 'file:local.db',
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+export function parseId(id: string): number | null {
+  const parsedId = id ? parseInt(id) : null;
+
+  if (parsedId && isNaN(parsedId)) throw new Error('invalid id');
+
+  return parsedId;
+}
