853619f7aa4b288298ff6358aaedcdec4ac220b7
ferdie bergado <ferdiebergado@gmail.com>
2025-12-24 20:01:48 +0800

    test: Add parseActivityCode

---
+++ netlify/functions/activity.test.ts
@@ -0,0 +1,52 @@
+import { describe, expect, it } from 'vitest';
+import { parseActivityCode, type FundSource } from './activity';
+
+describe('parseActivityCode', () => {
+  it('parses year correctly', () => {
+    const activityCode = 'AC-24-LDB-LDT-FLO-091';
+    const got = parseActivityCode(activityCode);
+    const want: FundSource = {
+      year: 2024,
+      appropriationType: 'current',
+      program: 'FLO',
+      mfoCode: '310300100003000',
+    };
+    expect(got).toEqual(want);
+  });
+
+  it('parses continuing funds correctly', () => {
+    const activityCode = 'AC-25-LDB-LDT-FLO-P091';
+    const got = parseActivityCode(activityCode);
+    const want: FundSource = {
+      year: 2025,
+      appropriationType: 'continuing',
+      program: 'FLO',
+      mfoCode: '310300100003000',
+    };
+    expect(got).toEqual(want);
+  });
+
+  it('parses current funds correctly', () => {
+    const activityCode = 'AC-25-LDB-LDT-FLO-091';
+    const got = parseActivityCode(activityCode);
+    const want: FundSource = {
+      year: 2025,
+      appropriationType: 'current',
+      program: 'FLO',
+      mfoCode: '310300100003000',
+    };
+    expect(got).toEqual(want);
+  });
+
+  it('parses program correctly', () => {
+    const activityCode = 'AC-24-LDB-LDT-BEC-091';
+    const got = parseActivityCode(activityCode);
+    const want: FundSource = {
+      year: 2024,
+      appropriationType: 'current',
+      program: 'BEC',
+      mfoCode: '310100100003000',
+    };
+    expect(got).toEqual(want);
+  });
+});
