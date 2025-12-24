c10d9badb2532bcad1bb42d4f6f302b6c6693900
ferdie bergado <ferdiebergado@gmail.com>
2025-12-24 11:38:46 +0800

    refactor: Create helpers for creating tags and applying patches

--- netlify/functions/certification.ts
+++ netlify/functions/certification.ts
@@ -3,7 +3,7 @@ import type { Config, Context } from '@netlify/functions';
 import { cert } from './cert';
 import { errorResponse, NotFoundError } from './errors';
 import { amountToWords, formatDate, formatToPhp, toDateRange } from './lib';
-import { getPayments, type PaymentTags } from './payments';
+import { getPayments, type Payment, type PaymentTags } from './payments';
 import { patchDoc } from './word';
 
 export const config: Config = {
@@ -26,42 +26,14 @@ export default async (_req: Request, ctx: Context) => {
     const firstPayment = payments[0];
     const activityCode = firstPayment.activityCode;
 
-    const tags: PaymentTags = {
-      payee: firstPayment.payee,
-      role: firstPayment.role,
-      activity: firstPayment.activity,
-      venue: firstPayment.venue,
-      end_date: formatDate(firstPayment.endDate),
-      amount: formatToPhp(firstPayment.honorarium),
-      tax: firstPayment.taxRate.toString(),
-      focal: firstPayment.focal.toLocaleUpperCase(),
-      position: firstPayment.position,
-      date: toDateRange(firstPayment.startDate, firstPayment.endDate),
-      amount_words: amountToWords(firstPayment.honorarium),
-    };
-
-    const firstCert = await patchDoc(cert, tags);
+    const firstCert = await patchTemplate(cert, firstPayment);
 
     if (!firstCert) throw new Error('failed to patch document');
 
     if (payments.length === 1) return docxResponse(firstCert, activityCode);
 
     const patchDocs = payments.slice(1).map(async payment => {
-      const tags: PaymentTags = {
-        payee: payment.payee,
-        role: payment.role,
-        activity: payment.activity,
-        venue: payment.venue,
-        end_date: formatDate(payment.endDate),
-        amount: formatToPhp(payment.honorarium),
-        tax: payment.taxRate.toString(),
-        focal: payment.focal.toLocaleUpperCase(),
-        position: payment.position,
-        date: toDateRange(payment.startDate, payment.endDate),
-        amount_words: amountToWords(payment.honorarium),
-      };
-
-      const patched = await patchDoc(cert, tags);
+      const patched = await patchTemplate(cert, payment);
       if (!patched) throw new Error('failed to patch document');
       return patched;
     });
@@ -79,6 +51,29 @@ export default async (_req: Request, ctx: Context) => {
     return errorResponse(error);
   }
 };
+
+function createTags(payment: Payment): PaymentTags {
+  const tags: PaymentTags = {
+    payee: payment.payee,
+    role: payment.role,
+    activity: payment.activity,
+    venue: payment.venue,
+    end_date: formatDate(payment.endDate),
+    amount: formatToPhp(payment.honorarium),
+    tax: payment.taxRate.toString(),
+    focal: payment.focal.toLocaleUpperCase(),
+    position: payment.position,
+    date: toDateRange(payment.startDate, payment.endDate),
+    amount_words: amountToWords(payment.honorarium),
+  };
+
+  return tags;
+}
+
+async function patchTemplate(template: string, payment: Payment) {
+  const tags = createTags(payment);
+  return await patchDoc(template, tags);
+}
 
 function docxResponse(body: Buffer, suffix: string) {
   return new Response(body, {
