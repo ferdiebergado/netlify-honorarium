464524f13a3bde88d97efbd45ab12ad347ea9ad3
ferdie bergado <ferdiebergado@gmail.com>
2025-12-24 20:01:14 +0800

    refactor: Create activity code parser

--- netlify/functions/activity.ts
+++ netlify/functions/activity.ts
@@ -9,3 +9,34 @@ export const schema = z.object({
   code: z.string().min(1),
   focalId: z.number().min(1),
 });
+
+const mfoCodes = {
+  BEC: '310100100003000',
+  ELLN: '310100100007000',
+  FLO: '310300100003000',
+} as const satisfies Record<string, string>;
+
+type Appropriation = 'current' | 'continuing';
+type Program = keyof typeof mfoCodes;
+
+export type FundSource = {
+  year: number;
+  appropriationType: Appropriation;
+  program: Program;
+  mfoCode: (typeof mfoCodes)[Program];
+};
+
+export function parseActivityCode(activityCode: string): FundSource {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, year, _bureau, _division, pap, code] = activityCode.split('-');
+
+  const program = pap as Program;
+  const mfoCode = mfoCodes[program];
+  const appropriationType: Appropriation = code.startsWith('P') ? 'continuing' : 'current';
+  return {
+    year: parseInt(year) + 2000,
+    appropriationType,
+    program,
+    mfoCode,
+  };
+}
