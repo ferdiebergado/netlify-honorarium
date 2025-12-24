4e99306a4ea4cd68575f6f94cadd4bd9f3607801
ferdie bergado <ferdiebergado@gmail.com>
2025-12-24 11:24:44 +0800

    refactor: Add list accounts hook

---
+++ src/features/accounts/accounts.ts
@@ -0,0 +1,27 @@
+import type { APIResponse } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+
+export type Account = {
+  id: number;
+  payeeId: number;
+  accountNo: string;
+  bank: string;
+  bankBranch: string;
+};
+
+async function getAccounts() {
+  const res = await fetch('/api/accounts');
+
+  const { message, data = [] } = (await res.json()) as APIResponse<Account[]>;
+
+  if (!res.ok) throw new Error(message);
+
+  return data;
+}
+
+export function useAccounts() {
+  return useQuery({
+    queryKey: ['accounts'],
+    queryFn: getAccounts,
+  });
+}
