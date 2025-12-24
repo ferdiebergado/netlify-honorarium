79324403e5f340455d339121b1ccc584b088afb0
ferdie bergado <ferdiebergado@gmail.com>
2025-12-24 10:31:49 +0800

    refactor: Merge all certifications of the activity

--- netlify/functions/certification.ts
+++ netlify/functions/certification.ts
@@ -1,3 +1,4 @@
+import { mergeDocx } from '@benedicte/docx-merge';
 import type { Config, Context } from '@netlify/functions';
 import { errorResponse, NotFoundError } from './errors';
 import { amountToWords, formatDate, formatToPhp, toDateRange } from './lib';
@@ -21,33 +22,68 @@ export default async (_req: Request, ctx: Context) => {
 
     if (payments.length === 0) throw new NotFoundError();
 
-    const payment = payments[0];
+    const firstPayment = payments[0];
+    const activityCode = firstPayment.activityCode;
 
     const tags: PaymentTags = {
-      payee: payment.payee,
-      role: payment.role,
-      activity: payment.activity,
-      venue: payment.venue,
-      end_date: formatDate(payment.endDate),
-      amount: formatToPhp(payment.honorarium),
-      tax: payment.taxRate.toString(),
-      focal: payment.focal.toLocaleUpperCase(),
-      position: payment.position,
-      date: toDateRange(payment.startDate, payment.endDate),
-      amount_words: amountToWords(payment.honorarium),
+      payee: firstPayment.payee,
+      role: firstPayment.role,
+      activity: firstPayment.activity,
+      venue: firstPayment.venue,
+      end_date: formatDate(firstPayment.endDate),
+      amount: formatToPhp(firstPayment.honorarium),
+      tax: firstPayment.taxRate.toString(),
+      focal: firstPayment.focal.toLocaleUpperCase(),
+      position: firstPayment.position,
+      date: toDateRange(firstPayment.startDate, firstPayment.endDate),
+      amount_words: amountToWords(firstPayment.honorarium),
     };
 
-    const cert = await patchDoc(tags);
+    const firstCert = await patchDoc(tags);
 
-    if (!cert) throw new Error('failed to patch document');
+    if (!firstCert) throw new Error('failed to patch document');
 
-    return new Response(cert, {
-      headers: {
-        'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'Content-Disposition': `attachment; filename="certification-ac-${activity_id}.docx"`,
-      },
+    if (payments.length === 1) return docxResponse(firstCert, activityCode);
+
+    const patchDocs = payments.slice(1).map(async payment => {
+      const tags: PaymentTags = {
+        payee: payment.payee,
+        role: payment.role,
+        activity: payment.activity,
+        venue: payment.venue,
+        end_date: formatDate(payment.endDate),
+        amount: formatToPhp(payment.honorarium),
+        tax: payment.taxRate.toString(),
+        focal: payment.focal.toLocaleUpperCase(),
+        position: payment.position,
+        date: toDateRange(payment.startDate, payment.endDate),
+        amount_words: amountToWords(payment.honorarium),
+      };
+
+      const patched = await patchDoc(tags);
+      if (!patched) throw new Error('failed to patch document');
+      return patched;
     });
+
+    const patchedDocs = await Promise.all(patchDocs);
+
+    const merged = patchedDocs.reduce((acc, curr) => {
+      const merged = mergeDocx(acc, curr, { insertEnd: true });
+      if (!merged) throw new Error('failed to merge documents');
+      return merged;
+    }, firstCert);
+
+    return docxResponse(merged, activityCode);
   } catch (error) {
     return errorResponse(error);
   }
 };
+
+function docxResponse(body: Buffer, suffix: string) {
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'Content-Disposition': `attachment; filename="certification-ac-${suffix}.docx"`,
+    },
+  });
+}
