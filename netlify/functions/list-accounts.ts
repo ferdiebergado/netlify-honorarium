17f6ec39485c9ec754999bc60a2248cbbc8350eb
ferdie bergado <ferdiebergado@gmail.com>
2025-12-27 21:26:10 +0800

    refactor: Move account types and helpers to list-accounts

--- netlify/functions/list-accounts.ts
+++ netlify/functions/list-accounts.ts
@@ -1,10 +1,11 @@
 import type { Config } from '@netlify/functions';
 import type { Account } from '../../src/shared/schema';
-import { deserializeDetails } from './accounts';
 import { turso } from './db';
 import { errorResponse } from './errors';
+import { toBuffer } from './lib';
+import { decrypt } from './security';
 
-type AccountRow = {
+export type AccountRow = {
   id: number;
   details: Buffer;
   payee_id: number;
@@ -19,14 +20,7 @@ export type AccountDetails = {
   accountName: string;
 };
 
-export const config: Config = {
-  method: 'GET',
-  path: '/api/accounts',
-};
-
-export default async () => {
-  try {
-    const sql = `
+export const accountsSql = `
   SELECT 
   a.id,
   a.details,
@@ -36,28 +30,40 @@ export default async () => {
   b.name AS bank_name
   FROM accounts a
   JOIN payees p ON p.id = a.payee_id
-  JOIN banks b ON b.id = a.bank_id 
-  ORDER BY b.name;`;
+  JOIN banks b ON b.id = a.bank_id`;
 
-    const { rows } = await turso.execute(sql);
+export const config: Config = {
+  method: 'GET',
+  path: '/api/accounts',
+};
 
-    const accounts = (rows as unknown as AccountRow[]).map(account => ({
-      ...account,
-      payeeId: account.payee_id,
-      payee: account.payee_name,
-      bank: account.bank_name,
-    }));
+export default async () => {
+  try {
+    const { rows } = await turso.execute(accountsSql);
 
-    const data: Account[] = accounts.map(account => {
-      const details = deserializeDetails(account.details);
-      return {
-        ...account,
-        ...details,
-      };
-    });
+    const data = (rows as unknown as AccountRow[]).map(rowToAccount);
 
     return Response.json({ data });
   } catch (error) {
     return errorResponse(error);
   }
 };
+
+export function rowToAccount(row: AccountRow): Account {
+  const details = deserializeDetails(row.details);
+
+  return {
+    id: row.id,
+    payeeId: row.payee_id,
+    payee: row.payee_name,
+    bank: row.bank_name,
+    ...details,
+  };
+}
+
+export function deserializeDetails(serialized: Buffer): AccountDetails {
+  const decrypted = decrypt(toBuffer(serialized));
+  const payload = decrypted.toString('utf-8');
+
+  return JSON.parse(payload) as AccountDetails;
+}
