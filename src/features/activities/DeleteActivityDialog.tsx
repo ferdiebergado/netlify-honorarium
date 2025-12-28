d05b0ce1fcc0d250707ca206ddff1873c2ea3fd9
ferdie bergado <ferdiebergado@gmail.com>
2025-12-28 18:49:03 +0800

    refactor: Remove success and error callback in the toast

--- src/features/activities/DeleteActivityDialog.tsx
+++ src/features/activities/DeleteActivityDialog.tsx
@@ -25,8 +25,6 @@ export default function DeleteActivityDialog({ activity }: DeleteActivityDialogP
   const handleAction = () => {
     toast.promise(deleteActivity(activity.id), {
       loading: 'Deleting Activity...',
-      success: ({ message }: { message: string }) => message,
-      error: (err: Error) => err.message,
     });
   };
 
