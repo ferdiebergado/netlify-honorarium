513414d77bf5d9d0219856c65afd4fc805d70c42
ferdie bergado <ferdiebergado@gmail.com>
2025-12-30 20:06:34 +0800

    refactor: Support dynamic forms

--- src/features/payees/PayeeInput.tsx
+++ src/features/payees/PayeeInput.tsx
@@ -1,33 +1,35 @@
 import { ComboboxField } from '@/components/ComboBox';
-import type { PaymentHookForm } from '../payments/payments';
+import type { FieldValues, Path, UseFormReturn } from 'react-hook-form';
 import CreatePayeeDialog from './CreatePayeeDialog';
 import { usePayees } from './payee';
 
-type PayeeInputProps = {
-  form: PaymentHookForm;
+type HasPayeeId = {
+  payeeId: number;
 };
 
-export default function PayeeInput({ form }: PayeeInputProps) {
-  const { isPending, isError, error, isSuccess, data: payees } = usePayees();
+type PayeeInputProps<T extends FieldValues & HasPayeeId> = {
+  form: UseFormReturn<T>;
+};
+
+export default function PayeeInput<T extends FieldValues & HasPayeeId>({
+  form,
+}: PayeeInputProps<T>) {
+  const { isPending, isError, error, data: payees = [] } = usePayees();
 
   return (
     <ComboboxField
       form={form}
-      name="payeeId"
+      name={'payeeId' as Path<T>}
       label="Payee"
       placeholder="Select payee..."
       searchPlaceholder="Search payee..."
       isPending={isPending}
       isError={isError}
       errorMessage={error?.message}
-      options={
-        isSuccess
-          ? payees.map(payee => ({
-              id: payee.id,
-              label: payee.name,
-            }))
-          : []
-      }
+      options={payees.map(payee => ({
+        id: payee.id,
+        label: payee.name,
+      }))}
     >
       <CreatePayeeDialog />
     </ComboboxField>
