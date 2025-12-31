d5df576c6392424240b2813f58e26e252718726e
ferdie bergado <ferdiebergado@gmail.com>
2025-12-31 12:05:42 +0800

    refactor: Add create account handler

---
+++ netlify/functions/create-account.ts
@@ -0,0 +1,32 @@
+import type { Config } from '@netlify/functions';
+import { createAccountSchema } from '../../src/shared/schema';
+import { turso } from '../db';
+import { errorResponse, ValidationError } from '../errors';
+import { encrypt } from '../security';
+import type { AccountDetails } from './list-accounts';
+
+export const config: Config = {
+  method: 'POST',
+  path: '/api/accounts',
+};
+
+export default async (req: Request) => {
+  try {
+    const body = await req.json();
+    const { error, data } = createAccountSchema.safeParse(body);
+
+    if (error) throw new ValidationError();
+
+    const { payeeId, bankId, bankBranch, accountNo, accountName } = data;
+
+    const details: AccountDetails = { bankBranch, accountName, accountNo };
+    const encryptedDetails = encrypt(Buffer.from(JSON.stringify(details), 'utf-8'));
+
+    const sql = 'INSERT INTO accounts (payee_id, bank_id, details) VALUES (?, ?, ?)';
+    await turso.execute(sql, [payeeId, bankId, encryptedDetails]);
+
+    return Response.json({ message: 'Account created.' }, { status: 201 });
+  } catch (error) {
+    return errorResponse(error);
+  }
+};
