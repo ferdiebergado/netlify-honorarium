3e9d436473d85d5148d286f64255f02a543b326b
ferdie bergado <ferdiebergado@gmail.com>
2025-12-31 20:40:06 +0800

    refactor: Add a link to Home

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyTitle } from './ui/empty';
 
 export default function NotFoundPage() {
@@ -5,15 +6,12 @@ export default function NotFoundPage() {
     <div className="flex min-h-screen items-center justify-center">
       <Empty>
         <EmptyHeader>
-          <EmptyTitle className="text-destructive text-2xl font-bold">Page Not Found</EmptyTitle>
-          <EmptyDescription>
-            The page you&apos;re looking for doesn&apos;t exist. Try searching for what you need
-            below.
-          </EmptyDescription>
+          <EmptyTitle className="text-2xl font-bold">Page Not Found</EmptyTitle>
+          <EmptyDescription>The page you&apos;re looking for doesn&apos;t exist.</EmptyDescription>
         </EmptyHeader>
         <EmptyContent>
           <EmptyDescription>
-            Need help? <a href="#">Contact support</a>
+            <Link to="/">Home</Link>
           </EmptyDescription>
         </EmptyContent>
       </Empty>
