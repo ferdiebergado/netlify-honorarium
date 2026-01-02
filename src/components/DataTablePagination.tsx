ea4ddc60cb70adaaccd17f8f6ef44fdfb6b762bd
ferdie bergado <ferdiebergado@gmail.com>
2026-01-02 08:28:12 +0800

    refactor: Move placeholder as child of SelectValue

--- src/components/DataTablePagination.tsx
+++ src/components/DataTablePagination.tsx
@@ -31,7 +31,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
             }}
           >
             <SelectTrigger className="h-8 w-[70px]">
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
+              <SelectValue>{table.getState().pagination.pageSize}</SelectValue>
             </SelectTrigger>
             <SelectContent side="top">
               {[10, 20, 25, 30, 40, 50].map(pageSize => (
