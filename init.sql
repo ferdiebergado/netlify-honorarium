575a62ee2ae622792c4b356055c2d60812049bbc
ferdie bergado <ferdiebergado@gmail.com>
2026-01-03 20:12:23 +0800

    refactor: Add commas between foreign key constraints of payments table

--- init.sql
+++ init.sql
@@ -1,7 +1,6 @@
-PRAGMA foreign_keys=OFF;
+PRAGMA foreign_keys = OFF;
 
 BEGIN TRANSACTION;
-
 
 CREATE TABLE IF NOT EXISTS activities (
   id INTEGER PRIMARY KEY,
@@ -109,9 +108,9 @@ CREATE TABLE IF NOT EXISTS payments (
   updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
   deleted_at TEXT,
   FOREIGN KEY (activity_id) REFERENCES activities (id),
-  FOREIGN KEY (payee_id) REFERENCES payees (id)
-  FOREIGN KEY (salary_id) REFERENCES salaries (id)
-  FOREIGN KEY (role_id) REFERENCES roles (id)
+  FOREIGN KEY (payee_id) REFERENCES payees (id),
+  FOREIGN KEY (salary_id) REFERENCES salaries (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id),
   FOREIGN KEY (account_id) REFERENCES accounts (id)
 );
 
@@ -137,17 +136,17 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-    id INTEGER PRIMARY KEY,
-    session_id TEXT UNIQUE NOT NULL,
-    user_id INTEGER NOT NULL,
-    ip_address TEXT,
-    user_agent TEXT,
-    expires_at TEXT NOT NULL,
-    last_active_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TEXT,
-    is_revoked INTEGER DEFAULT 0 CHECK (is_revoked IN (0, 1)),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+  id INTEGER PRIMARY KEY,
+  session_id TEXT UNIQUE NOT NULL,
+  user_id INTEGER NOT NULL,
+  ip_address TEXT,
+  user_agent TEXT,
+  expires_at TEXT NOT NULL,
+  last_active_at TEXT DEFAULT CURRENT_TIMESTAMP,
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TEXT,
+  is_revoked INTEGER DEFAULT 0 CHECK (is_revoked IN (0, 1)),
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 COMMIT;
