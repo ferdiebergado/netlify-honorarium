f7f7098d4a57e5e5d5f7d113a06efde6eabb8778
ferdie bergado <ferdiebergado@gmail.com>
2026-01-05 20:29:50 +0800

    test: Accept taxRate as argument to computeHonorarium

--- netlify/create-payment.test.ts
+++ netlify/create-payment.test.ts
@@ -4,6 +4,7 @@ import { computeHonorarium, type Honorarium } from './payments';
 type Given = {
   honorarium: number;
   salary: number;
+  taxRate: number;
 };
 
 type TestCase = { given: Given; want: Honorarium };
@@ -13,30 +14,36 @@ const testCases: TestCase[] = [
     given: {
       honorarium: 30000,
       salary: 41633.47,
+      taxRate: 10,
     },
     want: {
       hoursRendered: 32,
       actualHonorarium: 30642.23,
+      netHonorarium: 27000,
     },
   },
   {
     given: {
       honorarium: 25000,
       salary: 78642,
+      taxRate: 10,
     },
     want: {
       hoursRendered: 14,
       actualHonorarium: 25322.72,
+      netHonorarium: 22500,
     },
   },
   {
     given: {
       honorarium: 30000,
       salary: 200000,
+      taxRate: 10,
     },
     want: {
       hoursRendered: 8,
       actualHonorarium: 33210.53,
+      netHonorarium: 27000,
     },
   },
 ];
@@ -45,9 +52,9 @@ describe('computeHonorarium', () => {
   it.each(testCases)(
     'computes hours rendered and actual honorarium correctly',
     ({ given, want }) => {
-      const { honorarium, salary } = given;
+      const { honorarium, salary, taxRate } = given;
 
-      const got = computeHonorarium(honorarium, salary);
+      const got = computeHonorarium(honorarium, salary, taxRate);
       expect(got).toEqual(want);
     }
   );
