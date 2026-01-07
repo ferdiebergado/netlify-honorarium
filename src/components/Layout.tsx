0f3416edd77095e73ee52a111c7d70c7891969e3
ferdie bergado <ferdiebergado@gmail.com>
2026-01-07 18:37:39 +0800

    refactor: Show app splash when loading

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import { useAuth } from '@/features/auth/auth';
 import { Navigate, Outlet } from 'react-router';
 import AppSidebar from './AppSidebar';
-import Loader from './Loader';
 import { SidebarProvider, SidebarTrigger } from './ui/sidebar';
 
 export default function Layout() {
   const { isAuthenticated, isLoading, isError } = useAuth();
 
-  if (isLoading) return <Loader text="Logging in..." />;
+  if (isLoading)
+    return (
+      <main className="bg-background flex h-screen items-center justify-center">
+        <h1 className="text-primary text-5xl font-extrabold">{import.meta.env.VITE_APP_TITLE}</h1>
+      </main>
+    );
 
   if (isError || !isAuthenticated) return <Navigate to="/login" replace />;
 
