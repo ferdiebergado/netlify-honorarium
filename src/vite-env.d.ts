fab83a14c2e9fe2542f47991bfc1a344fd33eac9
ferdie bergado <ferdiebergado@gmail.com>
2026-01-07 18:40:59 +0800

    chore: Enable intellisense for vite import.meta.env

---
+++ src/vite-env.d.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // more env variables...
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
