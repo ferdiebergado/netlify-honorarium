a59897cabac105edec00645013a7e178554cc5f5
ferdie bergado <ferdiebergado@gmail.com>
2026-01-07 18:38:19 +0800

    refactor: Move class attribute to the Card component

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -3,12 +3,10 @@ import { Skeleton } from './ui/skeleton';
 
 export default function SkeletonCard() {
   return (
-    <Card>
-      <div className="ml-8 flex flex-col space-y-8 space-x-4">
-        <Skeleton className="h-9 w-1/4" />
-        <Skeleton className="h-28 w-9/10" />
-        <Skeleton className="h-9 w-3/7" />
-      </div>
+    <Card className="mt-8 flex flex-col gap-x-4 gap-y-8">
+      <Skeleton className="h-9 w-1/4" />
+      <Skeleton className="h-28 w-9/10" />
+      <Skeleton className="h-9 w-3/7" />
     </Card>
   );
 }
