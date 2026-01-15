05763697cbae3c479c6c831d272d0055bb31fc75
ferdie bergado <ferdiebergado@gmail.com>
2026-01-15 20:10:04 +0800

    refactor: Remove unused routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,11 @@
+import AuthLayout from '@/components/AuthLayout';
+import Layout from '@/components/Layout';
 import { lazy } from 'react';
 import type { RouteObject } from 'react-router';
-import AuthLayout from './components/AuthLayout';
-import Layout from './components/Layout';
 
 const ActivitiesPage = lazy(() => import('./features/activities/ActivitiesPage'));
-const PayeesPage = lazy(() => import('./features/payees/PayeesPage'));
-const PaymentsPage = lazy(() => import('./features/payments/PaymentsPage'));
 const ActivityPage = lazy(() => import('./features/activities/ActivityPage'));
-const LoginPage = lazy(() => import('@/components/LoginPage'));
-const AuthCallback = lazy(() => import('./features/auth/AuthCallback'));
+const LoginPage = lazy(() => import('@/features/auth/LoginPage'));
 const Dashboard = lazy(() => import('./components/Dashboard'));
 const NotFoundPage = lazy(() => import('@/components/NotFoundPage'));
 
@@ -29,14 +26,6 @@ export const routes = [
         path: 'activities',
         Component: ActivitiesPage,
       },
-      {
-        path: 'payees',
-        Component: PayeesPage,
-      },
-      {
-        path: 'payments',
-        Component: PaymentsPage,
-      },
     ],
   },
   {
@@ -46,10 +35,6 @@ export const routes = [
       {
         index: true,
         Component: LoginPage,
-      },
-      {
-        path: 'success',
-        Component: AuthCallback,
       },
     ],
   },
